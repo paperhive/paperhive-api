25d81d53c6be42a236ad086a7ba1de58241c4c0c
André Gaul <andre@gaul.io>
2014-10-21 14:06:36 +0200

    validate schemas

--- validate.js
+++ validate.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var glob = require('glob');
 var tv4 = require('tv4');
 var tv4formats = require('tv4-formats');
 
@@ -27,40 +28,46 @@ for (var i in schemas) {
 
 console.log();
 
-// read all examples and validate
-var files = fs.readdirSync('examples');
-for (var i in files) {
-  // skip hidden files
-  if (/^\./.exec(files[i])) {
-    continue;
-  }
+// read all schemas+examples and validate
+var dirs = ['schemas', 'examples'];
+for (var j in dirs) {
+  glob(dirs[j] + '/*.json', function (err, files) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
 
-  process.stdout.write('Validating \'examples/' + files[i] + '\'... ');
+    for (var i in files) {
+      var file = files[i];
+        
+      process.stdout.write('Validating \'' + file + '\'... ');
 
-  // parse example JSON
-  var example = JSON.parse(fs.readFileSync('examples/' + files[i], 'utf8'));
+      // parse example JSON
+      var example = JSON.parse(fs.readFileSync(file, 'utf8'));
 
-  // get schema
-  if (!example.$schema) {
-    console.log('Error: schema missing in example JSON.');
-    process.exit(1);
-  }
-  var schema = tv4.getSchema(example.$schema);
+      // get schema
+      if (!example.$schema) {
+        console.log('Error: schema missing.');
+        process.exit(1);
+      }
+      var schema = tv4.getSchema(example.$schema);
 
-  // validate!
-  var valid = tv4.validate(example, schema);
-  if (!valid) {
-    console.log('Error:\n' +
-      tv4.error.message + '\n' +
-      JSON.stringify(tv4.error, null, 2)
-    );
-    process.exit(1);
-  }
-  process.stdout.write('passed.\n');
+      // validate examples
+      var valid = tv4.validate(example, schema);
+      if (!valid) {
+        console.log('Error:\n' +
+          tv4.error.message + '\n' +
+          JSON.stringify(tv4.error, null, 2)
+        );
+        process.exit(1);
+      }
+      console.log('passed.');
+    }
+  });
 }
 
 // this is the end, my friend!
-console.log('\nAll examples passed the schema validation.');
+console.log('\nAll JSON files passed the schema validation.');
 
 // check for missing schemas
 for (var i in tv4.missing) {
